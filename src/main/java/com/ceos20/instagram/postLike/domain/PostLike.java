dc37c0096aafa87cd5582b56eb6cae02b7ef8515
gchaewon <conjs1@naver.com>
2024-09-21 18:20:29 +0900

    [feat] PostLike 도메인, 레포지토리 추가

---
+++ src/main/java/com/ceos20/instagram/postLike/domain/PostLike.java
@@ -0,0 +1,26 @@
+package com.ceos20.instagram.postLike.domain;
+
+import com.ceos20.instagram.post.domain.Post;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class PostLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_like_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="post_id")
+    private Post post;
+
+    private String image_url;
+
+}
