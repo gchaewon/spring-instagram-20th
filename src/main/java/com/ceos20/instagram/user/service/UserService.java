aae25cdfd5ee500455a532be9c78cd83d6e7109e
gchaewon <conjs1@naver.com>
2024-09-28 03:40:54 +0900

    [feat] 회원가입, 로그인 기능 추가

---
+++ src/main/java/com/ceos20/instagram/user/service/UserService.java
@@ -0,0 +1,59 @@
+package com.ceos20.instagram.user.service;
+
+import com.ceos20.instagram.user.domain.User;
+import com.ceos20.instagram.user.dto.UserLoginRequestDto;
+import com.ceos20.instagram.user.dto.UserLoginResponseDto;
+import com.ceos20.instagram.user.dto.UserRegisterRequestDto;
+import com.ceos20.instagram.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    // 회원 가입 메서드
+    @Transactional
+    public void register(UserRegisterRequestDto requestDto){
+        // 이메일 중복 체크
+        if (userRepository.existsByEmail(requestDto.getEmail())) {
+            throw new IllegalArgumentException("이미 사용 중인 이메일입니다: " + requestDto.getEmail());
+        }
+        // username 중복 체크
+        if(userRepository.existsByUsername(requestDto.getUsername())){
+            throw new IllegalArgumentException("이미 사용 중인 아이디입니다: " + requestDto.getUsername());
+        }
+        // 비밀번호 암호화
+        String encodedPassword = passwordEncoder.encode(requestDto.getPassword());
+
+        // 유저 저장
+        userRepository.save(requestDto.toEntity(encodedPassword));
+    }
+
+    // 로그인 메서드
+    @Transactional(readOnly = true)
+    public UserLoginResponseDto login(UserLoginRequestDto requestDto){
+        // username으로 사용자 조회
+        User user = userRepository.findByUsername(requestDto.getUsername()).orElse(null);
+
+        // username이 존재하지 않을 경우 예외 처리
+        if (user == null) {
+            throw new IllegalArgumentException("존재하지 않는 아이디입니다: " + requestDto.getUsername());
+        }
+
+        // 비밀번호 확인
+        if (!passwordEncoder.matches(requestDto.getPassword(), user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 맞지 않습니다.");
+        }
+
+        // 로그인 성공
+        return UserLoginResponseDto.from(user);
+    }
+}
