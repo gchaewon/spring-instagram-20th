5e6fb849eafd6fb12b8d4e7425c6bfd4079ffa3d
gchaewon <conjs1@naver.com>
2024-09-28 03:45:27 +0900

    [feat] 회원가입 유효성 검사를 위한 조회 메서드 추가

--- src/main/java/com/ceos20/instagram/user/repository/UserRepository.java
+++ src/main/java/com/ceos20/instagram/user/repository/UserRepository.java
@@ -3,5 +3,11 @@ package com.ceos20.instagram.user.repository;
 import com.ceos20.instagram.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
 }
