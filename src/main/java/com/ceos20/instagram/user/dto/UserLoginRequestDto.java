8c74d6d9d053cc2d9d7703f70d755d42ded155f0
gchaewon <conjs1@naver.com>
2024-09-28 03:41:14 +0900

    [feat] 로그인 Dto 추가

---
+++ src/main/java/com/ceos20/instagram/user/dto/UserLoginRequestDto.java
@@ -0,0 +1,12 @@
+package com.ceos20.instagram.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class UserLoginRequestDto {
+    @NotBlank(message = "아이디는 필수입니다.")
+    private String username;
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private String password;
+}
