1271a59a46cd9eac5a7d06a5cb5a5df8f166e784
gchaewon <conjs1@naver.com>
2024-09-28 22:43:22 +0900

    [feat] N+1 문제 확인, 해결 단위 테스트

--- src/test/java/com/ceos20/instagram/post/repository/PostRepositoryTest.java
+++ src/test/java/com/ceos20/instagram/post/repository/PostRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.ceos20.instagram.post.repository;
 
-import com.ceos20.instagram.comment.repository.CommentRepository;
 import com.ceos20.instagram.post.domain.CommentOption;
 import com.ceos20.instagram.post.domain.Post;
 import com.ceos20.instagram.user.domain.User;
@@ -26,6 +25,9 @@ public class PostRepositoryTest {
     private UserRepository userRepository;
 
     private User user;
+    private User user2;
+    private Post post1;
+    private Post post2;
 
     @BeforeEach
     void setUp() {
@@ -35,57 +37,60 @@ public class PostRepositoryTest {
                 .username("testuser")
                 .password("password")
                 .build();
-        userRepository.save(user);
-    }
+        user2 = User.builder()
+                .email("test2@example.com")
+                .username("testuser2")
+                .password("password")
+                .build();
 
-    @Test
-    @DisplayName("post 저장, 조회 테스트")
-    public void createAndGetPost() {
-        // given
-        // 포스트 생성
-        Post post = Post.builder()
+        userRepository.save(user);
+        userRepository.save(user2);
+
+        // 테스트용 포스트 생성
+        post1 = Post.builder()
                 .user(user)
-                .content("테스트 포스트")
+                .content("포스트 1")
+                .commentOption(CommentOption.ENABLED)
+                .build();
+        post2 = Post.builder()
+                .user(user2)
+                .content("포스트 2")
                 .commentOption(CommentOption.ENABLED)
                 .build();
 
-        postRepository.save(post);
-
-        // when
-        Post foundPost = postRepository.findById(post.getId()).orElse(null);
-
-        // then
-        assertThat(foundPost).isNotNull();
-        assertThat(foundPost.getContent()).isEqualTo("테스트 포스트");
+        postRepository.save(post1);
+        postRepository.save(post2);
     }
 
     @Test
-    @DisplayName("post 조회 테스트")
-    public void findAllPosts() {
-        // given
-        Post post1 = Post.builder().user(user).content("포스트 1").commentOption(CommentOption.ENABLED).build();
-        Post post2 = Post.builder().user(user).content("포스트 2").commentOption(CommentOption.ENABLED).build();
-        postRepository.save(post1);
-        postRepository.save(post2);
-
+    @DisplayName("N+1 문제 테스트")
+    public void nPlusOneProblemTest() {
         // when
         List<Post> posts = postRepository.findAll();
+
+        // 이 시점에서 User에 접근하여 N+1 문제를 유발
+        for (Post post : posts) {
+            String userEmail = post.getUser().getEmail(); // 이 부분에서 User 정보를 가져옴
+            System.out.println("User Email: " + userEmail); // User 정보를 출력
+        }
 
         // then
         assertThat(posts).hasSize(2);
     }
 
     @Test
-    @DisplayName("post 삭제 테스트")
-    public void deletePost() {
-        // given
-        Post post = Post.builder().user(user).content("포스트 삭제 테스트").commentOption(CommentOption.ENABLED).build();
-        postRepository.save(post);
-
+    @DisplayName("N+1 문제 해결 테스트")
+    public void nPlusOneSolveTest() {
         // when
-        postRepository.delete(post);
+        List<Post> posts = postRepository.findAllWithUsers();
+
+        // 이 시점에서 User에 접근하여 N+1 문제를 유발
+        for (Post post : posts) {
+            String userEmail = post.getUser().getEmail(); // 이 부분에서 User 정보를 가져옴
+            System.out.println("User Email: " + userEmail); // User 정보를 출력
+        }
 
         // then
-        assertThat(postRepository.findById(post.getId())).isEmpty();
+        assertThat(posts).hasSize(2);
     }
 }
