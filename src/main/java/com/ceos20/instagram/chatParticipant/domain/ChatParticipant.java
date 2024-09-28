d0611350a666f4cb8d25c084428cb6562bdde098
gchaewon <conjs1@naver.com>
2024-09-28 20:14:36 +0900

    [feat] ChatRoom과 연관 관계 추가

--- src/main/java/com/ceos20/instagram/chatParticipant/domain/ChatParticipant.java
+++ src/main/java/com/ceos20/instagram/chatParticipant/domain/ChatParticipant.java
@@ -1,5 +1,6 @@
 package com.ceos20.instagram.chatParticipant.domain;
 
+import com.ceos20.instagram.chatRoom.domain.ChatRoom;
 import com.ceos20.instagram.user.domain.User;
 
 import jakarta.persistence.*;
@@ -21,9 +22,14 @@ public class ChatParticipant {
     @JoinColumn(name="user_id")
     private User user;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
     @Builder
-    public ChatParticipant(Long id, User user){
+    public ChatParticipant(Long id, User user, ChatRoom chatRoom){
         this.id = id;
         this.user = user;
+        this.chatRoom = chatRoom;
     }
 }
