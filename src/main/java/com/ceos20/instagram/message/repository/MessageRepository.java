b29189c237df8ccfa8ae50a91d58836e9a66449f
gchaewon <conjs1@naver.com>
2024-09-28 21:29:16 +0900

    [feat] 채팅방 전체 메시지 조회 메서드 추가

--- src/main/java/com/ceos20/instagram/message/repository/MessageRepository.java
+++ src/main/java/com/ceos20/instagram/message/repository/MessageRepository.java
@@ -3,5 +3,9 @@ package com.ceos20.instagram.message.repository;
 import com.ceos20.instagram.message.domain.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    // 특정 채팅방의 전체 메시지 조회 메서드
+    List<Message> findAllByChatRoomId(Long chatRoomId);
 }
