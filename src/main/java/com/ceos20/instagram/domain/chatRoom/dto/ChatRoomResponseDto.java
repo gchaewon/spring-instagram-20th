c3c59d357211d78682911d712e79e1ceb53067f7
gchaewon <conjs1@naver.com>
2024-11-01 18:05:20 +0900

    [chore] 불필요한 필드 삭제

--- src/main/java/com/ceos20/instagram/domain/chatRoom/dto/ChatRoomResponseDto.java
+++ src/main/java/com/ceos20/instagram/domain/chatRoom/dto/ChatRoomResponseDto.java
@@ -11,19 +11,16 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class ChatRoomResponseDto {
     private Long id;
-    private LocalDateTime createdAt;
 
     @Builder
     public ChatRoomResponseDto(Long id, LocalDateTime createdAt) {
         this.id = id;
-        this.createdAt = createdAt;
     }
 
     // ChatRoom 엔티티를 DTO로 변환하는 메서드
     public static ChatRoomResponseDto from(ChatRoom chatRoom) {
         return ChatRoomResponseDto.builder()
                 .id(chatRoom.getId())
-                .createdAt(chatRoom.getCreatedAt())
                 .build();
     }
 }
