e6790d4afaaaf23c6db57f529a20f1ca44fa90d1
gchaewon <conjs1@naver.com>
2024-11-02 01:10:57 +0900

    [refactor] 정적 팩토리 메서드 적용

--- src/main/java/com/ceos20/instagram/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/com/ceos20/instagram/domain/comment/dto/CommentRequestDto.java
@@ -6,26 +6,21 @@ import com.ceos20.instagram.domain.user.domain.User;
 import lombok.Builder;
 import lombok.Getter;
 import jakarta.validation.constraints.NotBlank;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@Builder
 public class CommentRequestDto {
     @NotBlank(message = "댓글 내용은 필수입니다.")
     private String content;
-
-    @Builder
-    public CommentRequestDto(String content) {
-        this.content = content;
-    }
+    private Long parentCommentId;
 
     // Comment 엔티티로 변환하는 메서드
-    public Comment toEntity(Post post, User user, Comment parentComment) {
+    public static Comment toEntity(CommentRequestDto requestDto, Post post, User user, Comment parentComment) {
         return Comment.builder()
-                .post(post) // 댓글이 달리는 포스트
-                .user(user) // 댓글을 작성한 사용자
-                .content(this.content)
-                .parentComment(parentComment) // 부모 댓글 설정
+                .post(post)
+                .user(user)
+                .parentComment(parentComment)
+                .content(requestDto.content)
                 .build();
     }
 }
--- src/main/java/com/ceos20/instagram/domain/comment/dto/CommentResponseDto.java
+++ src/main/java/com/ceos20/instagram/domain/comment/dto/CommentResponseDto.java
@@ -3,31 +3,32 @@ package com.ceos20.instagram.domain.comment.dto;
 import com.ceos20.instagram.domain.comment.domain.Comment;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Getter
-@NoArgsConstructor
+@Builder
 public class CommentResponseDto {
     private Long id; // 댓글 고유 번호
-    private Long postId; // 댓글이 달린 포스트 ID
     private Long userId; // 댓글 작성자 고유 번호
-    private String content;
+    private Long postId; // 댓글이 달린 포스트 ID
+    private Long parentId; // 부모 댓글 아이디
 
-    @Builder
-    public CommentResponseDto(Long id, Long postId, Long userId, String content) {
-        this.id = id;
-        this.postId = postId;
-        this.userId = userId;
-        this.content = content;
-    }
+    private String content; // 댓글 내용
+    private LocalDateTime createdAt; // 생성 시간
+    private LocalDateTime modifiedAt; // 수정 시간
 
     // Comment 엔티티로부터 DTO로 변환하는 메서드
     public static CommentResponseDto from(Comment comment) {
         return CommentResponseDto.builder()
                 .id(comment.getId())
-                .postId(comment.getPost().getId())
                 .userId(comment.getUser().getId())
+                .postId(comment.getPost().getId())
+                // parentComment가 없으면 getId를 호출했을 때 null이 나오도록 설정
+                .parentId(comment.getParentComment() != null ? comment.getParentComment().getId() : null)
                 .content(comment.getContent())
+                .createdAt(comment.getCreatedAt())
+                .modifiedAt(comment.getModifiedAt())
                 .build();
     }
 }
