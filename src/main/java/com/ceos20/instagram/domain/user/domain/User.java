d7d105d0444addb7009790a18ece535c72c04b8a
gchaewon <conjs1@naver.com>
2024-11-09 18:00:00 +0900

    [feat] 유저 권한 추가

--- src/main/java/com/ceos20/instagram/domain/user/domain/User.java
+++ src/main/java/com/ceos20/instagram/domain/user/domain/User.java
@@ -6,23 +6,30 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
-@NoArgsConstructor (access = AccessLevel.PROTECTED)
 @Getter
-
-public class User{
+@NoArgsConstructor (access = AccessLevel.PROTECTED)
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
 
     @NotNull
-    @Column(length = 50)
+    @Column(length = 50, unique = true)
     private String username;
 
     @NotNull
-    @Column(length = 50, unique = true)
+    @Column(length = 50)
     private String nickname;
 
     @NotNull
@@ -36,6 +43,37 @@ public class User{
     @Column(length = 20)
     private String phone;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+    @Override
+    public boolean isEnabled(){
+        return true;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
     @Builder
     public User(Long id, String username, String nickname, String password, String email, String phone) {
         this.id = id;
@@ -45,6 +83,8 @@ public class User{
         this.email = email;
         this.phone = phone;
     }
+
+
 
 }
 
