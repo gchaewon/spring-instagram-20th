126a8122f479b519121207e19f52a9659ba1fb4d
gchaewon <conjs1@naver.com>
2024-11-09 18:03:00 +0900

    [feat] 로그인 ResponseDTO에 토큰 추가

--- src/main/java/com/ceos20/instagram/domain/user/dto/UserLoginResponseDto.java
+++ src/main/java/com/ceos20/instagram/domain/user/dto/UserLoginResponseDto.java
@@ -1,6 +1,7 @@
 package com.ceos20.instagram.domain.user.dto;
 
 import com.ceos20.instagram.domain.user.domain.User;
+import com.ceos20.instagram.global.config.jwt.JwtToken;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,17 +11,23 @@ import lombok.NoArgsConstructor;
 public class UserLoginResponseDto {
     private Long userId; // 유저 고유 번호
     private String username; // @user1234 같은 유저 id
+    private String accessToken;
+    private String refreshToken;
 
     @Builder
-    public UserLoginResponseDto(Long userId, String username){
+    public UserLoginResponseDto(Long userId, String username, String accessToken, String refreshToken){
         this.userId = userId;
         this.username = username;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
     }
 
-    public static UserLoginResponseDto from(User user){
+    public static UserLoginResponseDto from(User user, JwtToken jwtToken){
         return UserLoginResponseDto.builder()
                 .userId(user.getId())
                 .username(user.getUsername())
+                .accessToken(jwtToken.getAccessToken())
+                .refreshToken(jwtToken.getRefreshToken())
                 .build();
     }
 }
