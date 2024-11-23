d5263646e9fe1bbf69378f21ee081e198dcd18a6
gchaewon <conjs1@naver.com>
2024-11-23 17:11:06 +0900

    [chore] 허용 URI 분리

--- src/main/java/com/ceos20/instagram/global/config/SecurityConfig.java
+++ src/main/java/com/ceos20/instagram/global/config/SecurityConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -22,6 +25,20 @@ public class SecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
     private final CustomUserDetailsService customUserDetailsService;
 
+    private static final List<String> ALLOWED_URIS = Arrays.asList(
+            "/favicon.ico",
+            "/swagger-ui/index.html",
+            "/swagger-ui.html",
+            "/swagger-ui/.*",
+            "/v3/api-docs/.*",
+            "/swagger-resources/.*",
+            "/webjars/.*",
+            "/users/login",
+            "/users/id/.*",
+            "/users/register"
+    );
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         AuthenticationManager authenticationManager = authenticationManager(httpSecurity);
@@ -29,14 +46,12 @@ public class SecurityConfig {
         return httpSecurity
                 .httpBasic().disable()
                 .csrf().disable()
+                .formLogin().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/swagger-ui/**").permitAll()
-                .requestMatchers("/v3/api-docs/**").permitAll()
-                .requestMatchers("/users/login").permitAll()
-                .requestMatchers("/users/id/**").permitAll()
-                .requestMatchers("/users/register").permitAll()
+                .requestMatchers(ALLOWED_URIS.toArray(new String[0]))
+                .permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(jwtAuthenticationFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class)
@@ -50,6 +65,7 @@ public class SecurityConfig {
                 .jwtTokenProvider(jwtTokenProvider)
                 .authenticationManager(authenticationManager)
                 .customUserDetailsService(customUserDetailsService)
+                .allowedUris(ALLOWED_URIS)
                 .build();
     }
 
